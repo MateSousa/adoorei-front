1d66ee0e0439125b544a5f511c3caedba27c3e53
Mate <matessousa@outlook.com>
2022-04-11 15:32:41 -0300

    add: store accessor

---
+++ utils/store-accessor.ts
@@ -0,0 +1,15 @@
+import { Store } from 'vuex'
+import { getModule } from 'vuex-module-decorators'
+
+
+
+/* eslint-disable import/no-mutable-exports */
+
+const initializeStores = (store: Store<any>): void => {
+
+}
+
+export {
+    initializeStores,
+    
+}
