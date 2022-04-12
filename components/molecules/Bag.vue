11a4bc3cc91cd3b1145965755b118b133b5483fa
Mate <matessousa@outlook.com>
2022-04-12 08:08:53 -0300

    style: make count of product above the bag

--- components/molecules/Bag.vue
+++ components/molecules/Bag.vue
@@ -1,7 +1,7 @@
 <template>
-    <div class="bag">
+    <div class="bag flex align-center justify-center">
         <div class="img">
-            <font-awesome-icon icon="fa-solid fa-bag-shopping" />
+            <FontAwesomeIcon :icon="['fas', 'cart-shopping']" />
         </div>
         <div class="count">
             {{ count }}
@@ -13,13 +13,32 @@
     import Vue from 'vue'
 
     export default Vue.extend({
-        
+        data() {
+            return {
+                count: 0
+            }
+        }
     })
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
 
+    .img {
+        position: relative;
+        z-index: -1;
+    }
 
+    svg {
+        width: 25px;
+        height: 25px;
+    }
 
+    .count {
+        position: relative;
+        z-index: 10;
+        top: -5px;
+        right: 10px;
+        color: red;        
+    }
 </style>
-
+ 
