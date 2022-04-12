9509e4aa6658546980ead7a216037d563d8cf697
Mate <matessousa@outlook.com>
2022-04-12 14:47:33 -0300

    add: create product info and cover for more organization and reuse

---
+++ components/molecules/ProductCover.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="product-img">
+        <img :src="$product.image" :alt="`Image of ${$product.title}`" />
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { products } from '@/store'
+
+export default Vue.extend({
+    computed: {
+        $product() {
+            return products.$single
+        }
+    }
+})
+</script>
+
+<style scoped>
+
+</style>
---
+++ components/molecules/ProductInfo.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="product-info">
+        <div class="product-description">
+            <h2>{{ $product.description }}</h2>
+        </div>
+        <div class="product-price">
+            <p>{{ $product.price }}</p>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { products } from '@/store'
+
+export default Vue.extend({
+    computed: {
+        $product() {
+            return products.$single
+        }
+    }
+})
+</script>
+
+<style scoped>
+
+</style>
