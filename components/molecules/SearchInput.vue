a0f7d7cafff565ced7af315534957d00d23157e2
Mate <matessousa@outlook.com>
2022-04-11 23:33:43 -0300

    add: search input component

---
+++ components/molecules/SearchInput.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="search-input">
+        <Input placeholder="Search for a product" />
+    </div>
+</template>
+
+<script lang="ts">
+    import Vue from 'vue'
+
+    export default Vue.extend({
+        
+    })
+</script>
+
+<style scoped>
+
+</style>
