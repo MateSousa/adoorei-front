a3c9b88928c77ca544b1f5bc96eddab2ab7a4c4f
Mate <matessousa@outlook.com>
2022-04-12 08:49:33 -0300

    add: product store

---
+++ store/products.ts
@@ -0,0 +1,43 @@
+import { Module, VuexModule, Mutation, Action } from 'vuex-module-decorators'
+import { $axios } from '@/utils/nuxt-instance'
+import { Product } from  '@/models'
+
+
+
+@Module({ namespaced: true, name: 'products', stateFactory: true })
+export default class Products extends VuexModule { 
+
+    private products = {} as  Product[]
+    private product = {} as Product
+    
+    get $all() {
+        return this.products
+    }
+
+    get $single() {
+        return this.product
+    }
+
+    @Mutation
+    SET_PRODUCTS(products: Product[]) {
+        this.products = products
+    }
+
+    @Mutation
+    SET_PRODUCT(product: Product) {
+        this.product = product
+    }
+
+    @Action({ rawError: true })
+    public async index() {
+        const products = await $axios.$get('/products')
+        this.context.commit('SET_PRODUCTS', products)
+    }
+
+    @Action({ rawError: true })
+    public async show(id: number) {
+        const product = await $axios.$get(`/products/${id}`)
+        this.context.commit('SET_PRODUCT', product)
+    }
+
+}
