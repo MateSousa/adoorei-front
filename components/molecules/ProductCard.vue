d65456aaf9f3b426eb198459053c61ce1114e876
Mate <matessousa@outlook.com>
2022-04-12 13:12:39 -0300

    style: adding hover style and responsive cards

--- components/molecules/ProductCard.vue
+++ components/molecules/ProductCard.vue
@@ -1,12 +1,12 @@
 <template>
-    <NuxtLink class="hover:bg-red" :to="`/product/${product.id}`" >
+    <NuxtLink :to="`/product/${product.id}`" >
         <div
-        class="product-card flex flex-col justify-center items-center gap-10 ">
+        class="product-card flex flex-col justify-center items-center gap-10">
             <div class="product-img">
-                <img class="w-16 h-16" :src="product.image" :alt="`Image of ${product.title}`">
+                <img class="w-20 h-20" :src="product.image" :alt="`Image of ${product.title}`">
             </div>
             <div class="product-info">
-                <h3 class="text-lg sm:text-base font-bold">
+                <h3 class="text-base sm:text-sm font-bold p-2.5">
                     {{product.title}}
                 </h3>
             </div>
@@ -30,5 +30,27 @@ export default Vue.extend({
 </script>
 
 <style scoped>
+
+.product-card {
+    border: 1px solid #eaeaea;
+    border-radius: 5px;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+    transition: all 0.2s ease-in-out;
+    cursor: pointer;
+    width: 200px;
+    height: 250px;
+}
+
+.product-card:hover  {
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+    transform: translateY(-5px);
+}
+
+@media (min-width: 481px) {
+    .product-card {
+        width: 260px;
+        height: 270px;
+    }
+}
 
 </style>
