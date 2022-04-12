45490770de0022fda78dd646bb983e6642afc911
Mate <matessousa@outlook.com>
2022-04-12 16:17:00 -0300

    add:  container component

---
+++ components/bosons/Container.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="container">
+        <slot/>
+    </div>
+</template>
+
+
+
+<style  scoped>
+.container {
+  margin: 0 auto;
+  max-width: 1000px;
+  @media  (max-width: 480px) {
+    width: 90%;
+  }
+
+  @media  (min-width: 481px) {
+    width: 85%;
+  }
+}
+</style>
