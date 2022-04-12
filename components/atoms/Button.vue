7e76181064acb0b01524f31dd5c878c88e04961d
Mate <matessousa@outlook.com>
2022-04-12 15:47:03 -0300

    add:  create button component and adding some style

---
+++ components/atoms/Button.vue
@@ -0,0 +1,20 @@
+<template>
+    <button @click="$emit('click')">{{text}}</button>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+    props: {
+        text: {
+            type: String,
+            required: true
+        }
+    }        
+})
+</script>
+
+<style scoped>
+
+</style>
--- components/organisms/ProductDetails.vue
+++ components/organisms/ProductDetails.vue
@@ -1,18 +1,28 @@
 <template>
     <div class="product-details flex flex-col justify-center items-center">
-        <div class="product-title">
-            <h1>{{ $product.title }}</h1>
+
+        
+
+        <ProductCover class="w-56" />
+
+        <ProductInfo class="flex flex-col mt-5 gap-5" />
+
+        <div class="product-qnt flex justify-between items-center text-xl mt-5">
+            <span @click="qnt--" class="decrease">-</span>
+            <span class="quantity" >{{ qnt }}</span>
+            <span @click="qnt++" class="increase">+</span>
         </div>
-        <ProductCover />
-        <ProductInfo />
-        <div class="product-qnt">
-            <span @click="qnt--">-</span>
-            <input type="text" v-model="qnt" placeholder="0"/>
-            <span @click="qnt++" >+</span>
+
+        <div class="button-add-to-cart mt-5">
+            <Button text="Add to  cart" @click="addToCart" class="button-cart"/>
         </div>
-        <div class="button-add-to-cart">
-            <button @click="addToCart($product)">Add to cart</button>
+
+        <div class="button-to-checkout mt-5">
+            <NuxtLink to="/checkout">
+                <Button text="Go to checkout" class="checkout" />
+            </NuxtLink>
         </div>
+        
     </div>
 </template>
 
@@ -41,5 +51,61 @@ export default Vue.extend({
 </script>
 
 <style scoped>
+
+
+.button-cart, .checkout {
+    width: 450px;
+    height: 3rem;
+    border-radius: .75rem;
+    border: 1px solid #ccc;
+    text-transform: uppercase;
+    padding: 0.5rem;
+    font-size: 1.2rem;
+    font-weight: bold;
+    cursor: pointer;
+    transition: all 0.2s ease-in-out;
+}
+
+.button-cart {
+    background-color: #111;
+    color: #fff;
+}
+
+.checkout {
+    background-color: #808080;
+    color: #fff;
+}
+
+.button-cart:active {
+    background-color: #fd7e14;
+}
+
+
+.product-qnt {
+    width: 100px;
+    height: 3rem;
+    background-color: #111;
+    color: #fff;
+    border-radius: .25rem;
+    border: 1px solid #ccc;
+    text-transform: uppercase;
+    padding: .5rem;
+    font-size: 1.2rem;
+    font-weight: bold;
+    cursor: pointer;
+    transition: all 0.2s ease-in-out;
+}
+
+.product-qnt:active {
+    background-color: #fd7e14;
+}
+
+
+@media (min-width: 481px) {
+    .button-cart:hover, .checkout:hover {
+        background-color: #fd7e14;
+    }
+
+}
 
 </style>
