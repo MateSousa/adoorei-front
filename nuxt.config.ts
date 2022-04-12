64127d44564f5aa9989fa1fd60ae508a145bec97
Mate <matessousa@outlook.com>
2022-04-12 08:32:44 -0300

    style: merge main css in nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,7 +16,8 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
-    'normalize.css/normalize.css'
+    'normalize.css/normalize.css',
+    '@/assets/css/main.css',
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
