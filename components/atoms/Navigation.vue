af1a51a1e52eefe8f61a1deb77f8fc8c7785160e
Mate <matessousa@outlook.com>
2022-04-12 08:37:54 -0300

    chore: create nav component for more organization in header component

---
+++ components/atoms/Navigation.vue
@@ -0,0 +1,39 @@
+<template>
+    <nav class="nav">
+        <ul class="items flex justify-center items-center gap-20">
+            <li class="item">
+                <NuxtLink to="/">
+                    Home
+                </NuxtLink>
+            </li>
+            <li class="item">
+                Categories
+                <div class="categories">
+                    <ul class="categories-items" v-for="category in categories" :key="category.id" >
+                        <li class="categories-item">
+                            <NuxtLink :to="'/categories/' + category.id">
+                                {{ category.name }}
+                            </NuxtLink>
+                        </li>
+                    </ul>
+                </div>
+            </li>
+        </ul>
+    </nav>
+</template>
+
+<script lang="ts">
+    import Vue from 'vue'
+
+    export default Vue.extend({
+        
+    })
+</script>
+
+<style scoped>
+
+li {
+    font-size: 18px;
+}
+
+</style>
--- components/organisms/Header.vue
+++ components/organisms/Header.vue
@@ -1,27 +1,7 @@
 <template>
-    <header class="header">
-        <nav class="nav">
-            <ul class="items">
-                <li class="item">
-                    <NuxtLink to="/">
-                    Home
-                    </NuxtLink>
-                </li>
-                <li class="item">
-                    Categories
-                    <div class="categories">
-                        <ul class="categories-items" v-for="category in categories" :key="category.id" >
-                            <li class="categories-item">
-                                <NuxtLink :to="'/categories/' + category.id">
-                                    {{ category.name }}
-                                </NuxtLink>
-                            </li>
-                        </ul>
-                    </div>
-                </li>
-            </ul>
-        </nav>
-        <SearchInput />   
+    <header class="header flex justify-between items-center">
+        <Navigation />
+        <SearchInput class="sm:hidden" />   
         <Bag />     
     </header>
 </template>
