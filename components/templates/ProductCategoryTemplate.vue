922a74ce7403ec751d3d36c1552226f2fbf7190a
Mate <matessousa@outlook.com>
2022-04-12 17:28:40 -0300

    add: create category pages

---
+++ components/templates/ProductCategoryTemplate.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="home-template grid sm:grid-cols-2 grid-rows-5  justify-center items-center place-items-center gap-5">
+        {{$products}}
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { categories } from '@/store'
+
+    export default Vue.extend({
+        computed: {
+            $products() {
+                return categories.$single
+            }
+        }
+    })
+</script>
+
+<style scoped>
+
+@media (min-width: 481px) {
+    .home-template {
+        grid-template-columns: repeat(4, 300px);
+    }
+
+}
+
+</style>
---
+++ pages/category/_category.vue
@@ -0,0 +1,17 @@
+<template>
+    <ProductCategoryTemplate />
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { categories } from '@/store'
+
+export default Vue.extend({
+    layout: 'adoorei',
+    async asyncData({ params }) {
+        await categories.show(params.category as any)
+    }
+})
+</script>
+
+
