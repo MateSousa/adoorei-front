c16e70f60f522039d86fda6867a34c6201c7a2fb
Mate <matessousa@outlook.com>
2022-04-11 23:33:22 -0300

    add: input component

---
+++ components/atoms/Input.vue
@@ -0,0 +1,28 @@
+<template>
+    <input class="input" :placeholder="placeholder" :type="type"/>
+</template>
+
+<script lang="ts">
+    import Vue from 'vue'
+
+    export default Vue.extend({
+        props: {
+            placeholder: {
+                type: String,
+                default: ''
+            },
+            type: {
+                type: String,
+                default: 'text'
+            }
+        }
+    })
+</script>
+
+<style scoped>
+
+.input {
+    width: 100%;
+}
+
+</style>
