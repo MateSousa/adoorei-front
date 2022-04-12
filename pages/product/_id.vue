81d8a20cfb549c51a5ecc58dd62ed4c5b95df449
Mate <matessousa@outlook.com>
2022-04-12 14:44:33 -0300

    add: single product page

---
+++ pages/product/_id.vue
@@ -0,0 +1,16 @@
+<template>
+    <ProductDetailsTemplate />
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { products } from '@/store'
+
+export default Vue.extend({
+    async asyncData({ params }) {
+        await products.show({ id: params.id as any })
+    }
+})
+</script>
+
+
