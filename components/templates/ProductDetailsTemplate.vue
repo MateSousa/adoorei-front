4f3a5f6b77226235742c05a5968a40eac643bf71
Mate <matessousa@outlook.com>
2022-04-12 15:46:01 -0300

    add: adding product title

--- components/templates/ProductDetailsTemplate.vue
+++ components/templates/ProductDetailsTemplate.vue
@@ -1,15 +1,23 @@
 <template>
-    <div class="product-details-template">
+    <div class="product-details-template">        
+        <h1 class="text-2xl" >{{ $product.title }}</h1>
         <ProductDetails />
     </div>
 </template>
 
 <script lang="ts">
-    import Vue from 'vue'
+import Vue from 'vue'
+import { products } from '@/store'
 
-    export default Vue.extend({
+export default Vue.extend({
+    computed: {
+        $product() {
+            return products.$single
+        }
+    },
+
         
-    })
+})
 </script>
 
 <style scoped>
