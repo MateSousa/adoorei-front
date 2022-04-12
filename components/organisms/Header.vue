282e99bbb77a5c0dba0adb5fc4044b7739114759
Mate <matessousa@outlook.com>
2022-04-11 23:33:04 -0300

    add: header component

---
+++ components/organisms/Header.vue
@@ -0,0 +1,39 @@
+<template>
+    <header class="header">
+        <nav class="nav">
+            <ul class="items">
+                <li class="item">
+                    <NuxtLink to="/"">
+                    Home
+                    </NuxtLink>
+                </li>
+                <li class="item">
+                    Categories
+                    <div class="categories">
+                        <ul class="categories-items" v-for="category in categories" :key="category.id" >
+                            <li class="categories-item">
+                                <NuxtLink :to="'/categories/' + category.id">
+                                    {{ category.name }}
+                                </NuxtLink>
+                            </li>
+                        </ul>
+                    </div>
+                </li>
+            </ul>
+        </nav>
+        <SearchInput />   
+        <Bag />     
+    </header>
+</template>
+
+<script lang="ts">
+    import Vue from 'vue'
+
+    export default Vue.extend({
+        
+    })
+</script>
+
+<style scoped>
+
+</style>
