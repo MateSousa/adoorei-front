29a7143d62c1674502c247145e8a94fd35b43095
Mate <matessousa@outlook.com>
2022-04-12 16:17:33 -0300

    add: create default and adoorei  layouts

---
+++ layouts/adoorei.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="adoorei-layout">
+        <Header />
+        <main>
+            <Container>
+                <Nuxt/>
+            </Container>
+        </main>
+        <Footer />
+    </div>
+</template>
+
+<script lang="ts">
+    import Vue from 'vue'
+
+    export default Vue.extend({
+        
+    })
+</script>
+
+<style scoped>
+
+</style>
