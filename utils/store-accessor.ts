c8f4e18872ef499357779fbdbe15bb02648eff12
Mate <matessousa@outlook.com>
2022-04-12 08:51:28 -0300

    add: product store

--- utils/store-accessor.ts
+++ utils/store-accessor.ts
@@ -1,15 +1,17 @@
 import { Store } from 'vuex'
 import { getModule } from 'vuex-module-decorators'
 
-
+import Products from '@/store/products'
 
 /* eslint-disable import/no-mutable-exports */
+let products: Products
 
 const initializeStores = (store: Store<any>): void => {
+    products = getModule(Products, store)
 
 }
 
 export {
     initializeStores,
-    
+    products
 }
