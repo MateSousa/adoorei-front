761a6ee266bb11038242864feb6272b6bab68919
Mate <matessousa@outlook.com>
2022-04-12 08:16:15 -0300

    chore: adding tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {},
+    screens: {
+      'sm': { 'max': '480px' },
+      'md': { 'min': '481px', 'max': '768px' },
+      'lg': { 'min': '769px', 'max': '1024px' },
+      'xl': { 'min': '1025px' },
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
