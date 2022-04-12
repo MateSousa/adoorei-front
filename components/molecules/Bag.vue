704c3bb6ad7ebf3dd42263ae6b1ed40d14814348
Mate <matessousa@outlook.com>
2022-04-11 23:51:30 -0300

    update: bag component with icon and count of products in the bag

---
+++ components/molecules/Bag.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="bag">
+        <div class="img">
+            <font-awesome-icon icon="fa-solid fa-bag-shopping" />
+        </div>
+        <div class="count">
+            {{ count }}
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+    import Vue from 'vue'
+
+    export default Vue.extend({
+        
+    })
+</script>
+
+<style lang="scss" scoped>
+
+
+
+</style>
+
