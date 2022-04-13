902e7dc02f86111c737e96f19528d0db171bca77
Mate <matessousa@outlook.com>
2022-04-13 00:23:27 -0300

    add: create product filter component

---
+++ components/molecules/ProductFilter.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="product-filter flex items-center gap-10">
+        <p class="text-lg font-medium">Filter by</p>
+        <select name="filters" v-model="select" @change="onChange">
+            <option value="No filter" >No filter</option>
+            <option  value="title">Name</option>
+            <option  value="price" >Price</option>
+            <option  value="rating" >Rating</option>
+        </select>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+    data() {
+        return {
+            select: 'No filter'
+        }
+    },   
+    methods: {
+        onChange() {
+            this.$emit('change', this.select)
+        }
+    }
+
+})
+            
+</script>
+
+<style scoped>
+
+select  {
+    width: 300px;
+    height: 40px;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    padding: 0 10px;
+    font-size: 16px;
+    color: #333;
+    outline: none;
+}
+
+</style>
