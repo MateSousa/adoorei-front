a96bd4fdc2e2f243a66c7cf3ccd5d22abd85e40b
Mate <matessousa@outlook.com>
2022-04-17 14:12:29 -0300

    style: change css for more responsiev

--- components/atoms/Navigation.vue
+++ components/atoms/Navigation.vue
@@ -6,13 +6,13 @@
                     Home
                 </NuxtLink>
             </li>
-            <li class="item">
-                Categories
-                <div class="categories">
-                    <ul class="categories-items" v-for="category in categories" :key="category.id" >
-                        <li class="categories-item">
-                            <NuxtLink :to="'/categories/' + category.id">
-                                {{ category.name }}
+            <li class="item" @click="isOpen">
+                <p class="cursor-pointer">Categories</p>
+                <div class="categories "  v-show="show">
+                    <ul class="categories-items" v-for="category in $categories" :key="category" >
+                        <li class="categories-item flex flex-col items-start cursor-pointer">
+                            <NuxtLink :to="`/category/${category}`" @click="isOpen" class="p-5">
+                                {{ category }}
                             </NuxtLink>
                         </li>
                     </ul>
@@ -23,17 +23,45 @@
 </template>
 
 <script lang="ts">
-    import Vue from 'vue'
+import Vue from 'vue'
+import { categories } from '@/store'
 
-    export default Vue.extend({
-        
-    })
+export default Vue.extend({
+    data() {
+        return {
+            show: false,
+        }
+    },
+    computed: {
+        $categories() {
+            return categories.$all
+        }
+    },
+    methods: {
+        isOpen() {
+            this.show = !this.show
+        }
+    }
+})
 </script>
 
 <style scoped>
 
 li {
     font-size: 18px;
+}
+
+.categories {
+    position: absolute;
+    background: #fff;
+    z-index: 1;
+    border-right: 1px solid #e6e6e6;
+    border-bottom: 1px solid #e6e6e6;
+    border-left: 1px solid #e6e6e6;
+}
+
+a::first-letter {
+    text-transform: uppercase;
 }
 
 </style>
--- components/organisms/Header.vue
+++ components/organisms/Header.vue
@@ -2,22 +2,13 @@
     <header class="header h-16">
         <Container class="flex justify-between items-center pt-5">
             <Navigation />
-            <SearchInput class="sm:hidden" />   
+            <SearchInput class="sm:hidden input" />   
             <Bag />  
         </Container>   
     </header>
 </template>
 
-<script lang="ts">
-    import Vue from 'vue'
-
-    export default Vue.extend({
-        
-    })
-</script>
-
 <style scoped>
-
 .header {
     position: fixed;
     top: 0;
@@ -30,5 +21,20 @@
 .container  {
     max-width: 1200px;
 }
+
+.input {
+    width: 400px;
+    border-radius: 5px;
+    padding: 0 10px;
+    font-size: 1.2rem;
+    font-weight: bold;
+    color: #111;
+    transition: all 0.2s ease-in-out;
+    border:none;
+}
+
+
+
+
 
 </style>
