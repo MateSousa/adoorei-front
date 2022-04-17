e89cc2a736dec634d888bf21ddd3b7b021106a0a
Mate <matessousa@outlook.com>
2022-04-17 14:08:53 -0300

    add: nuxt-cookies

--- utils/nuxt-instance.ts
+++ utils/nuxt-instance.ts
@@ -1,10 +1,17 @@
 import type { NuxtAxiosInstance } from '@nuxtjs/axios'
+import { NuxtCookies } from 'cookie-universal-nuxt'
 
 /* eslint-disable import/no-mutable-exports */
 let $axios: NuxtAxiosInstance
+let $cookies: NuxtCookies
 
 export function initializeAxios(axiosInstance: NuxtAxiosInstance) {
   $axios = axiosInstance
 }
 
-export { $axios }
+
+export const initializeCookies = (cookiesInstance: NuxtCookies) => {
+  $cookies = cookiesInstance
+}
+
+export { $axios, $cookies }
