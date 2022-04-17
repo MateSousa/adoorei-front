43d886641879bc71f652f2255fed0b85b17471f5
Mate <matessousa@outlook.com>
2022-04-17 14:13:35 -0300

    add: logic for count product in the bag

--- components/molecules/Bag.vue
+++ components/molecules/Bag.vue
@@ -1,23 +1,31 @@
 <template>
-    <div class="bag flex justify-center">
-        <div class="img ">
-            <FontAwesomeIcon :icon="['fas', 'cart-shopping']" />
+    <NuxtLink to="/cart">
+        <div class="bag flex justify-center">
+            <div class="img ">
+                <FontAwesomeIcon :icon="['fas', 'cart-shopping']" />
+            </div>
+            <p class="count">
+                {{ $carts.length }}
+            </p>
         </div>
-        <div class="count">
-            {{ count }}
-        </div>
-    </div>
+    </NuxtLink>
 </template>
 
 <script lang="ts">
-    import Vue from 'vue'
+import Vue from 'vue'
+import { carts } from '@/store'
 
     export default Vue.extend({
         data() {
             return {
                 count: 0
             }
-        }
+        },
+        computed: {
+            $carts() {
+                return carts.$all
+            }
+        },
     })
 </script>
 
@@ -34,7 +42,7 @@
         z-index: 10;
         top: -5px;
         right: 10px;
-        color: red;        
+        color: #ff7300;        
     }
 </style>
  
