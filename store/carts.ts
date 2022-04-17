34568053cde6961912190e9aa576cfd22eaee700
Mate <matessousa@outlook.com>
2022-04-17 14:06:10 -0300

    update: update cart store with new action to work with cookies

--- store/carts.ts
+++ store/carts.ts
@@ -1,61 +1,129 @@
 import { Module, VuexModule, Mutation, Action } from 'vuex-module-decorators'
-import { Cart } from '@/models'
+import { $cookies } from '@/utils/nuxt-instance'
 
+interface Payload {
+    id: number
+    title: string
+    price: number
+    image: string
+    productQuantity: number
+}
 
+function updateLocalStorage(cart: Payload[]) {
+    $cookies.set('cart', JSON.stringify(cart))
+}
 
-@Module({ namespaced: true, name: 'cart', stateFactory: true })
+@Module({ namespaced: true, name: 'carts', stateFactory: true })
 export default class Carts extends VuexModule {
-    private cart = {} as Cart
+    private cart = [] as Payload[]
 
-    get $single() {
-        return this.cart
+    public get $all() {
+        return $cookies.get('cart')
+    }  
+
+
+    @Mutation
+    private SET_CART() {
+        const cookies = $cookies.get('cart')    
+        if (cookies) {
+            this.cart = cookies
+        } else {
+            this.cart = []
+            $cookies.set('cart', [])
+        }
     }
 
     @Mutation
-    SET_CART(cart: Cart) {
-        this.cart = cart
+    private ADD_CART(cart: Payload) {
+        const localStorage = $cookies.get('cart')
+        if (localStorage.length === 0) {
+            localStorage.push(cart)
+            updateLocalStorage(localStorage)    
+        } else if (localStorage.length > 0) {
+            const index = localStorage.findIndex(item => item.id === cart.id)
+            if (index > -1) {
+                localStorage[index].productQuantity += cart.productQuantity
+                localStorage[index].price += cart.price
+                updateLocalStorage(localStorage)
+            } else {
+                localStorage.push(cart)
+                updateLocalStorage(localStorage)
+            }            
+        }   
     }
 
     @Mutation
-    ADD_PRODUCT(product: Cart['products'][0]) {
-        this.cart.products.push(product)
-    }
-
-    @Mutation
-    REMOVE_PRODUCT(product: Cart['products'][0]) {
-        this.cart.products.splice(this.cart.products.indexOf(product), 1)
-    }
-
-    @Action({ rawError: true })
-    public async index() {
-        const cart = this.cart || [] 
-        return cart
-    }
-
-    @Action({ rawError: true })
-    public async addProduct(product: Cart['products'][0]) {
-        this.context.commit('ADD_PRODUCT', product)
-    }
-
-    @Action({ rawError: true })
-    public async removeProduct(product: Cart['products'][0]) {
-        this.context.commit('REMOVE_PRODUCT', product)
-    }
-
-    
-    
-
-
+    private ADD_ONE_PRODUCT(cart: Payload) {
+        const localStorage = $cookies.get('cart')
+        const index = localStorage.findIndex(item => item.id === cart.id)
+        if (index > -1) {
+            localStorage[index].price += localStorage[index].price / localStorage[index].productQuantity        
+            localStorage[index].productQuantity += 1
+            updateLocalStorage(localStorage)
+        } else {
+            return `Something went wrong`
+        }
         
-        
-
-
-
-
-
-
+    }        
     
 
+    @Mutation
+    private REMOVE_ONE_PRODUCT(cart: Payload) {
+       const localStorage = $cookies.get('cart')
+        const index = localStorage.findIndex(item => item.id === cart.id)
+        if (index > -1) {
+            localStorage[index].price -= localStorage[index].price / localStorage[index].productQuantity
+            localStorage[index].productQuantity -= 1
+            updateLocalStorage(localStorage)
+        } else {
+            return `Something went wrong`
+        }
+        
+    }
+
+    @Mutation
+    private REMOVE_CART_PRODUCT(cart: Payload) {
+        const localStorage = $cookies.get('cart')
+        const index = localStorage.findIndex(item => item.id === cart.id)
+        if (index > -1) {
+            localStorage.splice(index, 1)
+            updateLocalStorage(localStorage)
+        } else {
+            return `Something went wrong`
+        }
+
+    }
+
+    @Action({ rawError: true })
+    public index() {
+       this.context.commit('SET_CART')
+    }
+
+    @Action({ rawError: true })
+    public addProduct(payload: Payload) {
+        const product = payload
+        this.context.commit('ADD_CART', product)
+    }
+
+    @Action({ rawError: true }) 
+    public addOneProduct(payload: Payload) {
+        const product = payload        
+        this.context.commit('ADD_ONE_PRODUCT', product)
+    }
+
+    @Action({ rawError: true })
+    public removeProduct(payload: Payload) {
+        const product = payload
+        this.context.commit('REMOVE_CART_PRODUCT', product)
+    }
+
+    @Action({ rawError: true })
+    public removeOneProduct(payload: Payload) {
+        const product = payload
+        this.context.commit('REMOVE_ONE_PRODUCT', product)
+    }
+
+    
 
 
 
