58ae738f69e90e518128f3f452fb11eb9605c490
Mate <matessousa@outlook.com>
2022-04-18 12:21:01 -0300

    add: create error layout

---
+++ layouts/error.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="error-layout">
+        <Header />
+            <main class="main mt-32">
+                <Container>
+                    <div class="error-msg flex flex-col justify-center items-center text-3xl sm:text-lg un">
+                        <h1 class="text-orange">Oops!</h1>
+                        <h2>We can't seem to find the page you are looking for.</h2>
+                        <a href="/" class="underline">Go to home page</a>
+                    </div>
+                </Container>
+            </main>
+        <Footer />
+    </div>
+</template>
+
+
+<style scoped>
+
+.footer {
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+    text-align: center;
+    padding: 1rem;
+    
+}
+
+</style>
+
